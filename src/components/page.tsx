e79c57b61fddb9f4f24154f056e626cfadce79b1
Michael <morrisamike@gmail.com>
2022-12-06 13:28:18 -0500

    removed axios for fetch

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -5,45 +5,15 @@ import Card from './cardRefresh';
 
 
  const Page:React.FC<{pageinfo?:PageInfo}> = (props) => {
- const [cardList,setCardList] = useState <PageComponent[]>([]);
-
-  //generate Cards based on the list component ID's 
-  const generateCards = ()=>{
-    props.pageinfo?.lists.forEach(
-      (list)=>{
-        list.components.forEach(
-          (compID)=>{
-            props.pageinfo?.components.forEach(
-              (c)=>{
-                //didnt like ternary if for some reason ???
-                if(c.id===compID){
-                  updateCards(cardList.push(c));
-                }
-              }
-            )
-          }
-        )
-      }
-    )
-  }
 
   useEffect(()=>{
-    generateCards();
-   },[props.pageinfo])
 
-  const updateCards=(updatedData:any)=>{
-    let tempArray =[...cardList];
-    tempArray.push(updatedData);
-    setCardList(tempArray);
-  }
+   },[])
 
+   
     return (
-      <div className='page '>
-        {cardList.map(
-          (c)=>(
-            <Card component={c} key={Math.random()}/>
-          )
-        )}
+      <div className='page'>
+       
      </div>
     ); 
   }
